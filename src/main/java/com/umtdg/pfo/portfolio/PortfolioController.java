89e018d6369ae19f0a88c04745bec6b087ff0ce2
Umut Dag <umu7d9@gmail.com>
2025-09-28 21:47:41 +0300

    fix: remove unused import `Collectors` from `PortfolioController`

--- src/main/java/com/umtdg/pfo/portfolio/PortfolioController.java
+++ src/main/java/com/umtdg/pfo/portfolio/PortfolioController.java
@@ -7,7 +7,6 @@ import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.UUID;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
