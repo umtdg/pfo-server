dc4c3796a0df10af8bb85ca27da7e8e1f4ef0f41
Umut Dag <umu7d9@gmail.com>
2025-11-04 11:35:56 +0300

    test: add tests for NotFoundException

---
+++ src/test/java/com/umtdg/pfo/TestNotFoundException.java
@@ -0,0 +1,45 @@
+package com.umtdg.pfo;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ProblemDetail;
+
+import com.umtdg.pfo.exception.NotFoundException;
+
+class TestNotFoundException {
+    @Test
+    void givenEntityAndId_shouldConstructNotFoundException() {
+        String expectedMessage = "Could not find 'Entity' with id 'EntityID'";
+        HttpStatusCode expectedStatus = HttpStatus.NOT_FOUND;
+
+        NotFoundException exception = new NotFoundException("Entity", "EntityID");
+
+        assertEquals(expectedMessage, exception.getMessage());
+        assertEquals(expectedStatus, exception.getStatusCode());
+        assertEquals(
+            ProblemDetail.forStatusAndDetail(expectedStatus, expectedMessage),
+            exception.getBody()
+        );
+    }
+
+    @Test
+    void givenEntityAndListOfIds_shouldConstructNotFoundException() {
+        List<String> ids = List.of("Id1", "Id2", "Id3");
+        String expectedMessage = "Could not find 'Entity' with ids Id1, Id2, Id3";
+        HttpStatusCode expectedStatus = HttpStatus.NOT_FOUND;
+
+        NotFoundException exception = new NotFoundException("Entity", ids);
+
+        assertEquals(expectedMessage, exception.getMessage());
+        assertEquals(expectedStatus, exception.getStatusCode());
+        assertEquals(
+            ProblemDetail.forStatusAndDetail(expectedStatus, expectedMessage),
+            exception.getBody()
+        );
+    }
+}
